0713d960dc7efa5ffc57239fed21983f8c727804
Unknown <mihailm956@gmail.com>
2017-09-17 00:34:28 +0300

    FINISHED - Angular 2 Components & Routes

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { Router, RouterModule, Routes } from "@angular/router";
+
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgModule } from '@angular/core';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
+
+const appRoutes:Routes = [
+  {path:'', component: HomeComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'login', component: LoginComponent},
+  {path:'dashboard', component: DashboardComponent},
+  {path:'profile', component: ProfileComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
